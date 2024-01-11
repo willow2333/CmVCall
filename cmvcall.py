abbcc6cd0f0c700bbfb37abc4ea9f1a4718e0ff3
willow <47717534+willow2333@users.noreply.github.com>
2024-01-11 14:32:53 +0800

    Add files via upload

---
+++ cmvcall.py
@@ -0,0 +1,16 @@
+from run import Run
+from pathlib import Path
+
+def main():
+    classrun = Run()
+    args = classrun.get_args()
+    inputdir = Path(args.input)
+    cinput = args.cinput
+    correct = args.CORRECT
+    a1 = float(args.a1)
+    a2 = float(args.a2)
+    FC = float(args.FC)
+    depth = float(args.depth)
+    classrun.run(inputdir,correct,a1,a2,FC,depth,cinput)
+if __name__=="__main__":
+    main()
